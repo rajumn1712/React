0f6075d8a68ed21797f102c2c6c98b01af94d704
Nagaraju <nagarajumandadapu3@gmail.com>
2020-10-29 21:56:39 +0530

    added form

--- src/components/FormsFolder/FormsComponent.js
+++ src/components/FormsFolder/FormsComponent.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
+import Aux from '../../hoc/Auxillary';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,13 +16,27 @@ const useStyles = makeStyles((theme) => ({
 export default function FormComponent() {
   const classes = useStyles();
 
+  const [useForm,SetUseForm] = useState({
+    Id:'',FirstName:'',LastName:'',UserName:''
+})
+
+const handleChange = (event) =>{
+    const input = event.target;
+   const user = {...useForm};
+   user[input.name] = input.value;
+   SetUseForm(user);
+}
+
   return (
+      <Aux>
+          <h5>User Form</h5>
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Id" name="Id" />
-      <TextField id="standard-basic" label="First Name" name="FirstName" />
-      <TextField id="standard-basic" label="Last Name" name="LastName" />
-      <TextField id="standard-basic" label="Email" name="UserName" />
+      <TextField id="standard-basic" label="Id" name="Id" onChange={(event)=>handleChange(event)}/>
+      <TextField id="standard-basic" label="First Name" name="FirstName" onChange={(event)=>handleChange(event)}/>
+      <TextField id="standard-basic" label="Last Name" name="LastName" onChange={(event)=>handleChange(event)}/>
+      <TextField id="standard-basic" label="Email" name="UserName" onChange={(event)=>handleChange(event)}/>
       <Button color="primary">Submit</Button>
     </form>
+      </Aux>
   );
 }
